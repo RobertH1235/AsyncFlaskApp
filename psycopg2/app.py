f48dcdf16a2b1274a6ec0a832712f8ca11687895
RobertH1235 <robert.hartles@ontoforce.com>
2023-06-21 15:47:23 +0200

    first commit

---
+++ psycopg2/app.py
@@ -0,0 +1,26 @@
+# ./psycopg2/app.py
+
+from gevent import monkey
+monkey.patch_all()
+
+import os
+
+import psycopg2
+import requests
+from flask import Flask, request
+
+api_port = os.environ['PORT_API']
+api_url = f'http://slow_api:{api_port}/'
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = psycopg2.connect(user="example", password="example", host="postgres")
+    delay = float(request.args.get('delay') or 1)
+    resp = requests.get(f'{api_url}?delay={delay/2}')
+
+    cur = conn.cursor()
+    cur.execute("SELECT NOW(), pg_sleep(%s)", (delay/2,))
+
+    return 'Hi there! {} {}'.format(resp.text, cur.fetchall()[0])
