d2d5f82d98e0f7e21cffb41b5118e132f966a89b
RobertH1235 <robert.hartles@ontoforce.com>
2023-06-29 15:14:21 +0100

    adding new upload/download app and gevent attempt

---
+++ download_app/gevent-uwsgi/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, send_from_directory
+from gevent.pywsgi import WSGIServer
+from gevent import monkey
+
+monkey.patch_all()
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part in the request"
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return "No file selected"
+
+    if file and allowed_file(file.filename):
+        file.save(f"{app.config['UPLOAD_FOLDER']}/{file.filename}")
+        return "File uploaded successfully"
+    else:
+        return "Invalid file"
+
+
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('', 8000), app)
+    http_server.serve_forever()
